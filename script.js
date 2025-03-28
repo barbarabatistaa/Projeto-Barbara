31bbdf4f32c243e526f5211da25638410a2b9a3c
barbarabatistaa <164499792+barbarabatistaa@users.noreply.github.com>
2025-03-28 09:24:39 -0300

    f

--- script.js
+++ script.js
@@ -24,4 +24,8 @@ document.addEventListener('DOMContentLoaded', function(){
 
     });
 
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+})
+
 });
